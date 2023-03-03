0c6942225a71a4186e29887a9a94a2f9ee18b680
KaineKriegar <you@example.com>
2023-03-03 14:23:55 -0800

    Adding Font

--- Guten-widget/widget.py
+++ Guten-widget/widget.py
@@ -1,7 +1,7 @@
 from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QWidget,
                              QLabel, QHBoxLayout, QLineEdit, 
                              QPushButton, QTextEdit)
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QFont 
 from PyQt6.QtCore import Qt
 import sys
  
@@ -20,19 +20,22 @@ class Window(QWidget):
  # create our widgets
         title_label = QLabel("Guten Search Widget")
         title_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+        title_label.setFont(QFont("Calibri", 22))
 
         description = "search the gutenberg project by title,"
         description += "author, or subject"
         description_label = QLabel(description)
+        description_label.setFont(QFont("Calibri", 14))
 
         search_layout = QHBoxLayout()
         self.search_field = QLineEdit()
         search_button = QPushButton("search")
+        search_button.setFont(QFont("Calibri", 12))
         search_layout.addWidget(self.search_field)
         search_layout.addWidget(search_button)
 
         results_text = QTextEdit("Results")
-
+        results_text.setFont(QFont("Calibri", 12))
 
         layout.addWidget(title_label)
         layout.addWidget(description_label)
